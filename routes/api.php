888b030ffe4fd8c7b6ee92fb817157e8e55b4b46
Szymon Bogdał <bogdal.symon@gmail.com>
2025-05-22 00:04:34 +0200

    Add API routes for movies, series, and genres with locale middleware

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\MovieController;
+use App\Http\Controllers\Api\SerieController;
+use App\Http\Controllers\Api\GenreController;
+use App\Http\Middleware\SetLocale;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware(SetLocale::class)->group(function () {
+    Route::get('/movies', [MovieController::class, 'index']);
+    Route::get('/movies/{id}', [MovieController::class, 'show']);
+
+    Route::get('/series', [SerieController::class, 'index']);
+    Route::get('/series/{id}', [SerieController::class, 'show']);
+
+    Route::get('/genres', [GenreController::class, 'index']);
+    Route::get('/genres/{id}', [GenreController::class, 'show']);
+});
